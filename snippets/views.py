2ab36c7602026d887369cc3c5dc9cbcad5a07834
moocstudent <deadzq@qq.com>
2023-01-11 22:02:59 +0800

    Using generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -3,43 +3,22 @@ from snippets.serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
 
-
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    """
-    List all code snippets, or create a new snippet.
-    """
-
-    # 获取list ListModelMixin
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+"""
+Using the mixin classes we've rewritten the views to use slightly less code than before, 
+but we can go one step further. REST framework provides a set of already mixed-in generic 
+views that we can use to trim down our views.py module even more.
+"""
 
 """
-we're using the GenericAPIView class to provide the core functionality, 
-and adding in mixins to provide the .retrieve(), .update() and .destroy() actions.
+List all code snippets, or create a new snippet.
 """
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    """
-    Retrieve, update or delete a code snippet.
-    """
 
-    # 单个获取retrieve RetrieveModelMixin
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+"""
+Retrieve, update or delete a code snippet.
+"""
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
